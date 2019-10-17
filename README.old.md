4f66cf560f59e8c79150e9eab6e32ebfac0f68c6
Affiliatethis <chris@onecrookedtree.com>
2019-10-17 15:30:02 -0400

    One of the first commits.  In the process of adding an under construction page.

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Debbieshair
+Hair Salon Site
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
+
+function App() {
+  return (
+<h1>Test</h1>
+  )
+}
+
+export default App;
