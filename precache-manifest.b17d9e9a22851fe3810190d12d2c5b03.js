92cad9cf42b06925495347b8a612575a589eda12
Affiliatethis <chris@onecrookedtree.com>
2019-10-21 15:36:57 -0400

    Updates

---
+++ precache-manifest.b17d9e9a22851fe3810190d12d2c5b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181009cde51471f2af4a4287b0f18195",
+    "url": "/debbishair/index.html"
+  },
+  {
+    "revision": "87c6e18bc168f5421d0a",
+    "url": "/debbishair/static/css/2.ab7d64b5.chunk.css"
+  },
+  {
+    "revision": "a3c36754bd2b9f583e66",
+    "url": "/debbishair/static/css/main.50563a07.chunk.css"
+  },
+  {
+    "revision": "87c6e18bc168f5421d0a",
+    "url": "/debbishair/static/js/2.906e04a3.chunk.js"
+  },
+  {
+    "revision": "a3c36754bd2b9f583e66",
+    "url": "/debbishair/static/js/main.fa36e5d2.chunk.js"
+  },
+  {
+    "revision": "50b027b1414cbc957bb5",
+    "url": "/debbishair/static/js/runtime-main.45ab76fe.js"
+  }
+]);
