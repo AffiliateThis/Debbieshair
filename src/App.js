efc830548b7a84604158d52247ef8e5b900bc977
Affiliatethis <chris@onecrookedtree.com>
2019-10-21 20:55:22 -0400

    2nd push. Under  Construction site loads, but gh pages isn't working properly yet.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar'
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import UnderConstruction from "react-under-construction";
+import "react-under-construction/build/css/index.css";
 
-function App() {
-  return (
-<h1>Test</h1>
-  )
-}
+const App = () => (
+  <UnderConstruction
+    background={{
+      image: "https://static.pexels.com/photos/259698/pexels-photo-259698.jpeg",
+      textColor: "#fff",
+      overlay: {
+        color: "#000",
+        opacity: ".5"
+      }
+    }}
+    logo={{
+      src: "",
+      alt: "alt text"
+    }}
+    title={{
+      className:"Title",
+      text: "The Hair Studios",
+      
+    }}
+    description={{
+      text:
+        "Our website is under construction. We will soon be live.  Check back for details",
+      style: {
+        maxWidth: "440px"
+      }
+    }}
+    // subscribe={{
+    //   placeholder: 'Enter your email',
+    //   buttonText: 'Subscribe',
+    //   onSubmit: (value) => {
+    //     console.log('user typed email :', value);
+    //   }
+    // }}
+  />
+);
 
 export default App;
